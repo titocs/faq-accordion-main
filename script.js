925bd61fd059e74c1134780f04f081cf67ab9e16
Hfn <55591161+titocs@users.noreply.github.com>
2021-11-19 10:18:29 +0700

    Add files via upload

---
+++ script.js
@@ -0,0 +1,28 @@
+"use strict"
+
+document.addEventListener("DOMContentLoaded", () => {
+    const cardHeaders = document.getElementsByClassName("card-heading");
+
+    for (const cardHeader of cardHeaders) {
+        const cardBody = cardHeader.nextElementSibling;
+        cardBody.classList.add("card-body-none"); 
+        cardHeader.addEventListener("click", (event) => {
+            for (const cardHeader of cardHeaders) {
+                const cardBody = cardHeader.nextElementSibling;
+                const cardHeaderImg = cardHeader.querySelector("img");
+                const cardHeadingH2 = cardHeader.querySelector("h2");
+                if (cardHeader != event.currentTarget) {
+                    cardBody.classList.add("card-body-none");
+                    cardHeaderImg.classList.remove("img-rotation");
+           
+                    cardHeadingH2.classList.remove("card-heading-h2");
+                } else {
+                    cardBody.classList.toggle("card-body-none");
+                    cardHeaderImg.classList.toggle("img-rotation");
+           
+                    cardHeadingH2.classList.toggle("card-heading-h2");
+                }
+            }
+        });
+    };
+});
